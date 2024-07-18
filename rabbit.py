a1926d7b976abee03cc919901d0cd73b128d0a02
Hugh Wells <hugh@crablab.co.uk>
2024-07-18 14:45:46 +0100

    fix: replace poor locking with offloaded tasks

---
+++ rabbit.py
@@ -0,0 +1,28 @@
+from celery import Celery
+from serial import SerialException
+import logging
+from infosys import display
+import os
+
+broker_url = os.environ['BROKER_URL']
+display_tasks = Celery('tasks', broker=broker_url)
+
+@display_tasks.task
+def time_background():
+    display = display()
+
+    try:
+      display.print_time()
+    except SerialException as e:
+      logging.warn('Time could not get lock')
+      return
+    
+@display_tasks.task
+def text_background(msg: str, effect: str, wipe: bool):
+    display = display()
+
+    try:
+      display.write_text(msg, effect, wipe)
+    except SerialException as e:
+      logging.warn('Text could not get lock')
+      return
